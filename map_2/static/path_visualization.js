249f14b9a6f7326bf0c41ac5373b1c863841e561
Dj <debanikroy92@gmail.com>
2024-05-03 15:47:30 +0900

    visualizeMap(mapData);

--- map_2/static/path_visualization.js
+++ map_2/static/path_visualization.js
@@ -1,6 +1,7 @@
 var maps = {}; // Dictionary to store maps and their canvas elements
 var canvas, ctx, mapData, scaleX, scaleY;
 var mapName;
+
 // ROS connection setup (assuming ROSLIB is already included)
 var ros = new ROSLIB.Ros({
   url: 'ws://localhost:9090'  // Replace with your ROS bridge server address
@@ -33,13 +34,11 @@ var mapview = new ROSLIB.Topic({
   messageType: 'nav_msgs/OccupancyGrid'  // Adjust based on your map message type
 });
 
-
 var pathSubscriber = new ROSLIB.Topic({
     ros : ros,
     name : '/plan',
     messageType : 'nav_msgs/Path'
 });
-
 
 mapview.subscribe(function(map_msg) {
   mapName = mapview.name; // Assuming topic name represents map name
@@ -49,21 +48,28 @@ mapview.subscribe(function(map_msg) {
   if (!maps[mapName]) {
     canvas = createCanvas(mapName);
     maps[mapName].ctx = canvas.getContext('2d');
+    console.log(`Inside: ${mapName}`);
+
+    canvas = maps[mapName].canvas;
+    ctx = maps[mapName].ctx;
+
+    // Update canvas dimensions based on map data
+    canvas.width = 480;
+    canvas.height = 480;
+
+    // Clear previous map visualization (optional)
+    // clearCanvas(mapName);
+
+    scaleX = canvas.width / map_msg.info.width;
+    scaleY = canvas.height / map_msg.info.height;
+    
+    visualizeMap(map_msg);
   }
 
-  canvas = maps[mapName].canvas;
-  ctx = maps[mapName].ctx;
+  
+});
 
-  // Update canvas dimensions based on map data
-  canvas.width = 480;
-  canvas.height = 480;
-
-  // Clear previous map visualization (optional)
-  clearCanvas(mapName);
-
-  scaleX = 480 / map_msg.info.width;
-  scaleY = 480 / map_msg.info.height;
-
+function visualizeMap(map_msg) {
   for (var y = 0; y < map_msg.info.height; y++) {
     for (var x = 0; x < map_msg.info.width; x++) {
       var index = x + y * map_msg.info.width;
@@ -82,32 +88,39 @@ mapview.subscribe(function(map_msg) {
       ctx.fillRect(x * scaleX, y * scaleY, scaleX, scaleY);
     }
   }
-});
+}
 
-pathSubscriber.subscribe(function(pathMsg) {
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);  // Clear previous path
-    visualizePath(pathMsg.poses);
+
+pathSubscriber.subscribe(function(pathMsg) { 
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  visualizePath(pathMsg.poses);
+  visualizeMap(mapData);
     // console.log(pathMsg.poses);
   });
 
 function visualizePath(poses) {
-    ctx.beginPath();
-    const pose1 = poses[0].pose.position;
-    const imageCoords1 = mapToImageCoordinates(pose1.x, pose1.y);
-    ctx.moveTo(imageCoords1.x, imageCoords1.y);
-
-    for (let i = 1; i < poses.length - 1; i++) {
+    // ctx.strokeStyle = 'green';
+    const darkGreen = '#006400'; // You can adjust the hex code as needed
+    ctx.strokeStyle = darkGreen;
+    ctx.lineWidth = 10;
+    for (let i = 0; i < poses.length - 1; i++) {
+        const pose1 = poses[i].pose.position;
         const pose2 = poses[i + 1].pose.position;
+        
+        // console.log(`Path Segment ${i+1}:`);
+        // console.log(`Pose 1: (${pose1.x}, ${pose1.y})`);
+        // console.log(`Pose 2: (${pose2.x}, ${pose2.y})`);
+
+        const imageCoords1 = mapToImageCoordinates(pose1.x, pose1.y);
         const imageCoords2 = mapToImageCoordinates(pose2.x, pose2.y);
+
+        ctx.beginPath();
+        ctx.moveTo(imageCoords1.x, imageCoords1.y);
         ctx.lineTo(imageCoords2.x, imageCoords2.y);
         ctx.stroke();
-
-        // console.log("Image coordinates1:", imageCoords1);
-        // console.log("Image coordinates2:", imageCoords2);
-        // console.log("--------------------");
     }
+    
 }
-
 
 function mapToImageCoordinates(robot_x, robot_y) {
     // Extract map information
@@ -121,7 +134,7 @@ function mapToImageCoordinates(robot_x, robot_y) {
     const pixel_x = Math.floor((robot_x - map_origin_x) / map_resolution);
     const pixel_y = Math.floor(image_height - (robot_y - map_origin_y) / map_resolution);  // Invert y-axis
 
-    return { x: pixel_x*scaleX, y: pixel_y*scaleY};
+    return { x: pixel_x * scaleX, y: pixel_y * scaleY };
 }
 
 // ROS connection events
